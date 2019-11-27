d3b66d4901f317a7e0ec58448dfa9bd5a7286e61
Vadym Kaidalov <vadym.kaidalov@gmail.com>
2019-11-27 18:03:54 +0200

    Create new user as admin feature

--- nure_web_lab/user_list.py
+++ nure_web_lab/user_list.py
@@ -1,6 +1,7 @@
 from flask import (
-    abort, Blueprint, redirect, render_template, request, url_for
+    abort, Blueprint, flash, redirect, render_template, request, url_for
 )
+from werkzeug.security import generate_password_hash
 
 from . import db
 from auth import admin_required, login_required
@@ -46,6 +47,56 @@ def get_user_list():
 
     users = cursor.fetchall()
     return render_template('user_list/user_list.html', users=users, group=group)
+
+
+@bp.route('/create', methods=('GET', 'POST'))
+@admin_required
+def create_user_item():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        full_name = request.form.get('full_name')
+        is_admin = bool(request.form.get('is_admin'))
+
+        connection = db.get_db_connection()
+        cursor = connection.cursor()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif len(username) > db.USER_USERNAME_MAX_LENGTH:
+            error = 'Username is too long.'
+        elif not password:
+            error = 'Password is required.'
+        elif not full_name:
+            error = 'Full name is required.'
+        elif len(full_name) > db.USER_FULL_NAME_MAX_LENGTH:
+            error = 'Full name is too long.'
+        else:
+            cursor.execute(
+                'SELECT id FROM "user" WHERE username = %s', (username,)
+            )
+            if cursor.fetchone() is not None:
+                error = f'User {username} is already created.'
+
+        if error is None:
+            cursor.execute(
+                'INSERT INTO "user"'
+                ' (username, full_name, password, is_admin)'
+                ' VALUES (%s, %s, %s, %s) RETURNING id',
+                (username, full_name,
+                 generate_password_hash(password), is_admin)
+            )
+            user = cursor.fetchone()
+
+            connection.commit()
+            return redirect(url_for(
+                'user_list.get_user_item', user_id=user['id']
+            ))
+
+        flash(error)
+
+    return render_template('user_list/user_create.html')
 
 
 def get_user_or_404(user_id: int, cursor):
